5ff0a11af20b3435862c77ef6e4f1c78f037459c
Jai singh <jaisingh0335@gmail.com>
2022-12-15 19:14:08 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-kanban-fire-codelab';
+
+  constructor(private dialog: MatDialog) {}
+
+  todo: Task[] = [
+    {
+      title: 'Create kanban board',
+      description: 'create kanban board following google codelab'
+    },
+    {
+      title: 'add firebase',
+      description: 'add firebase to kanban board'
+    }
+  ];
+
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+
+  editTask(list: string, task: Task): void { }
+  
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.previousContainer.data || !event.container.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    )
+  }
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {},
+      }
+    });
+
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult | undefined) => {
+        if (!result) {
+          return;
+        }
+        this.todo.push(result.task);
+    })
+  }
 }
